27e394ed002f714cefd432d7d3703568ae59e116
IAmTerror <cyrilberthout@gmail.com>
2021-02-23 22:25:55 +0100

    header.php

--- index.php
+++ index.php
@@ -11,28 +11,7 @@
 <body>
 <div class="screen">
     <div class="screen-reflection"></div>
-    <header class="header">
-        <div class="header-top">
-            <a href="#" class="header-top-active">STAT</a>
-            <a href="#">INV</a>
-            <a href="#">DATA</a>
-            <a href="#">MAP</a>
-            <a href="#">RADIO</a>
-        </div>
-        <div class="header-lines-container">
-            <div class="header-line-bottom header-line-bottom-left"></div>
-            <div class="header-line-vertical header-line-vertical-left"></div>
-            <div class="header-line-hook header-line-hook-left"></div>
-            <div class="header-line-hook header-line-hook-right"></div>
-            <div class="header-line-vertical header-line-vertical-right"></div>
-            <div class="header-line-bottom header-line-bottom-right"></div>
-        </div>
-        <div class="sub-header sub-header-stat">
-            <a href="#" class="sub-header-active-tab">STATUS</a>
-            <a href="#" class="sub-header-inactive-tab-near">SPECIAL</a>
-            <a href="#" class="sub-header-inactive-tab-far">PERKS</a>
-        </div>
-    </header>
+    <?php include("header.php"); ?>
     <section class="core-display">
         <div class="vault-boy-container">
             <img id="vault-boy-walking"
