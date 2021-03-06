159a99c79b348bfe8759e39eef8dfe51209c804f
IAmTerror <cyrilberthout@gmail.com>
2021-03-06 20:08:03 +0100

    S.P.E.C.I.A.L. HTML WIP

--- stat_special.php
+++ stat_special.php
@@ -1,40 +1,42 @@
 <section class="core-display" id="stat-special-section">
-    <div class="special-attributes">
-        <div class="special-attribute-box">
-            <p class="special-attribute">Strength</p>
-            <p class="special-value">2</p>
+    <div class="special-container">
+        <div class="special-attributes">
+            <div class="special-attribute-box">
+                <p class="special-attribute">Strength</p>
+                <p class="special-value">2</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Perception</p>
+                <p class="special-value">9</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Endurance</p>
+                <p class="special-value">8</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Charisma</p>
+                <p class="special-value">3</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Intelligence</p>
+                <p class="special-value">9</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Agility</p>
+                <p class="special-value">4</p>
+            </div>
+            <div class="special-attribute-box">
+                <p class="special-attribute">Luck</p>
+                <p class="special-value">10</p>
+            </div>
         </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Perception</p>
-            <p class="special-value">9</p>
+        <div class="special-right-column">
+            <img src="" alt="coming soon">
+            <p class="special-attribute-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
+                vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
+                officia quibusdam deleniti eos cupiditate dolore doloribus!
+            </p>
         </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Endurance</p>
-            <p class="special-value">8</p>
-        </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Charisma</p>
-            <p class="special-value">3</p>
-        </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Intelligence</p>
-            <p class="special-value">9</p>
-        </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Agility</p>
-            <p class="special-value">4</p>
-        </div>
-        <div class="special-attribute-box">
-            <p class="special-attribute">Luck</p>
-            <p class="special-value">10</p>
-        </div>
-    </div>
-    <div class="stat-special-right-column">
-        <img src="img/vault_boy_walking.gif" alt="a fallout vault boy walking">
-        <p class="special-attribute-description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
-            vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
-            officia quibusdam deleniti eos cupiditate dolore doloribus!
-        </p>
     </div>
 </section>
