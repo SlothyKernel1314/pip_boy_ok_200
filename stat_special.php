9d19c58370c90f4b9327464f7feec6c0ca74b68d
IAmTerror <cyrilberthout@gmail.com>
2021-03-06 18:58:08 +0100

    S.P.E.C.I.A.L. HTML

--- stat_special.php
+++ stat_special.php
@@ -29,5 +29,12 @@
             <p class="special-value">10</p>
         </div>
     </div>
-    <div class="stat-special-right-column"></div>
+    <div class="stat-special-right-column">
+        <img src="img/vault_boy_walking.gif" alt="a fallout vault boy walking">
+        <p class="special-attribute-description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae
+            vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda
+            officia quibusdam deleniti eos cupiditate dolore doloribus!
+        </p>
+    </div>
 </section>
