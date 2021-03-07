d3e5e8ae5ca6f87f39d6968e2b1e818de3fc634e
IAmTerror <cyrilberthout@gmail.com>
2021-03-07 22:04:08 +0100

    JS special.js

--- js/special.js
+++ js/special.js
@@ -5,6 +5,20 @@ var special_attribute_names = ['strength', 'perception', 'endurance',
     'charisma', 'intelligence', 'agility', 'luck'];
 
 
+function activeAttributeBox(box) {
+    for (var i=0 ; i < special_attribute_boxes.length ; i++) {
+        special_attribute_boxes[i].classList.remove('special-attribute-box-active');
+    }
+
+    for (var i=0 ; i < special_attribute_names.length ; i++) {
+        if(box.id === 'special-attribute-box-' + special_attribute_names[i] + '') {
+            document.getElementById('special-attribute-box-' + special_attribute_names[i]).
+            classList.add('special-attribute-box-active');
+        }
+    }
+}
+
+
 function changeSpecialDescription(box) {
     for (var i=0 ; i < special_attribute_descriptions.length ; i++) {
         special_attribute_descriptions[i].classList.remove('special-attribute-description-active');
@@ -19,9 +33,10 @@ function changeSpecialDescription(box) {
 }
 
 
-// processing execution when a mouseover is detected on a special attribute boc
+// processing execution when a mouseover is detected on a special attribute box
 for (var i=0 ; i < special_attribute_boxes.length ; i++) {
     special_attribute_boxes[i].addEventListener('mouseover', function (evt) {
         changeSpecialDescription(this);
+        activeAttributeBox(this);
     })
 }
