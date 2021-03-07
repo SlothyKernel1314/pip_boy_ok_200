59bb621e7b30d0ce9befd72997461817ae6df535
IAmTerror <cyrilberthout@gmail.com>
2021-03-07 16:56:07 +0100

    special.js preparation

--- stat_special.php
+++ stat_special.php
@@ -1,57 +1,57 @@
 <section class="core-display" id="stat-special-section">
     <div class="special-container">
         <div class="special-attributes">
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-strength">
                 <p class="special-attribute">Strength</p>
                 <p class="special-value">2</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-perception">
                 <p class="special-attribute">Perception</p>
                 <p class="special-value">9</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-endurance">
                 <p class="special-attribute">Endurance</p>
                 <p class="special-value">8</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-charisma">
                 <p class="special-attribute">Charisma</p>
                 <p class="special-value">3</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-intelligence">
                 <p class="special-attribute">Intelligence</p>
                 <p class="special-value">9</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-agility">
                 <p class="special-attribute">Agility</p>
                 <p class="special-value">4</p>
             </div>
-            <div class="special-attribute-box">
+            <div class="special-attribute-box special-attribute-box-luck">
                 <p class="special-attribute">Luck</p>
                 <p class="special-value">10</p>
             </div>
         </div>
         <div class="special-right-column">
             <img src="/img/vault_boy_thumbs_up.gif" id="vault-boy-thumbs-up" alt="a fallout vault boy thumbs up">
-            <p class="special-attribute-description-active">
+            <p class="special-attribute-description-active" id="special-attribute-description-strenght">
                 Suffisant pour cliquer sur ENTER et envoyer un git push --force plus destructeur qu'une
                 patate de forain.
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-perception">
                 Perception Description Placeholder
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-endurance">
                 Endurance Description Placeholder
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-charisma">
                 Charisma Description Placeholder
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-intelligence">
                 Intelligence Description Placeholder
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-agility">
                 Agility Description Placeholder
             </p>
-            <p class="special-attribute-description">
+            <p class="special-attribute-description" id="special-attribute-description-luck">
                 Luck Description Placeholder
             </p>
         </div>
