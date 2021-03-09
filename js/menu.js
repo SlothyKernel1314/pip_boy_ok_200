f51ae44969f7a4832cba6fdb7656e61d1dc31910
IAmTerror <cyrilberthout@gmail.com>
2021-03-09 22:29:38 +0100

    perks WIP / menu.js (ex special.js) refactoring for multiples pages abstraction done

--- js/menu.js
+++ js/menu.js
@@ -8,28 +8,18 @@ var special_attribute_names = ['strength', 'perception', 'endurance',
 var perk_attribute_names = ['p1', 'p2', 'p3'];
 
 
-function activeBox(box, boxes, attribute_names, box_class_basename, active_box_class) {
+function activeMenu(box, boxes, descriptions, attribute_names, box_class_basename, description_class_basename,
+                    active_box_class, active_description_class) {
     for (var i=0 ; i < boxes.length ; i++) {
         boxes[i].classList.remove(active_box_class);
     }
-
+    for (var i=0 ; i < descriptions.length ; i++) {
+        descriptions[i].classList.remove(active_description_class);
+    }
     for (var i=0 ; i < attribute_names.length ; i++) {
         if(box.id === box_class_basename + '-' + attribute_names[i] + '') {
             document.getElementById(box_class_basename + '-' + attribute_names[i]).
             classList.add(active_box_class);
-        }
-    }
-}
-
-
-function changeDescription(box, descriptions, attribute_names, box_class_basename,
-    description_class_basename, active_description_class) {
-    for (var i=0 ; i < descriptions.length ; i++) {
-        descriptions[i].classList.remove(active_description_class);
-    }
-
-    for (var i=0 ; i < attribute_names.length ; i++) {
-        if(box.id === box_class_basename + '-' + attribute_names[i] + '') {
             document.getElementById(description_class_basename + '-' + attribute_names[i]).
             classList.add(active_description_class);
         }
@@ -40,13 +30,11 @@ function changeDescription(box, descriptions, attribute_names, box_class_basenam
 // processing execution when a mouseover is detected on a special attribute box
 for (var i=0 ; i < special_attribute_boxes.length ; i++) {
     special_attribute_boxes[i].addEventListener('mouseover', function (evt) {
-        activeBox(this, special_attribute_boxes, special_attribute_names,
-            'special-attribute-box',
-            'special-attribute-box-active');
-        changeDescription(this, special_attribute_descriptions, special_attribute_names,
+        activeMenu(this, special_attribute_boxes, special_attribute_descriptions, special_attribute_names,
             'special-attribute-box',
             'special-attribute-description',
-            'special-attribute-description-active');
+            'special-attribute-box-active',
+            'special-attribute-description-active')
 
     })
 }
@@ -55,12 +43,10 @@ for (var i=0 ; i < special_attribute_boxes.length ; i++) {
 // processing execution when a mouseover is detected on a perk attribute box
 for (var i=0 ; i < perk_attribute_boxes.length ; i++) {
     perk_attribute_boxes[i].addEventListener('mouseover', function (evt) {
-        activeBox(this, perk_attribute_boxes, perk_attribute_names,
-            'perk-attribute-box',
-            'perk-attribute-box-active');
-        changeDescription(this, perk_attribute_descriptions, perk_attribute_names,
+        activeMenu(this, perk_attribute_boxes, perk_attribute_descriptions, perk_attribute_names,
             'perk-attribute-box',
             'perk-attribute-description',
-            'perk-attribute-description-active');
+            'perk-attribute-box-active',
+            'perk-attribute-description-active')
     })
 }
