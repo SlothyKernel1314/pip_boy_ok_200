f78d2c43570a74a96ba13296feae276d38a38f9e
IAmTerror <cyrilberthout@gmail.com>
2021-03-10 22:51:53 +0100

    JS var ---> transform to let (i'm a newbie in JS...)

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,22 @@
-var special_attribute_boxes = document.querySelectorAll('.special-attribute-box');
-var special_attribute_descriptions = document.querySelectorAll('.special-attribute-description');
-var perk_attribute_boxes = document.querySelectorAll('.perk-attribute-box');
-var perk_attribute_descriptions = document.querySelectorAll('.perk-attribute-description');
+let special_attribute_boxes = document.querySelectorAll('.special-attribute-box');
+let special_attribute_descriptions = document.querySelectorAll('.special-attribute-description');
+let perk_attribute_boxes = document.querySelectorAll('.perk-attribute-box');
+let perk_attribute_descriptions = document.querySelectorAll('.perk-attribute-description');
 
-var special_attribute_names = ['strength', 'perception', 'endurance',
+let special_attribute_names = ['strength', 'perception', 'endurance',
     'charisma', 'intelligence', 'agility', 'luck'];
-var perk_attribute_names = ['p1', 'p2', 'p3'];
+let perk_attribute_names = ['p1', 'p2', 'p3'];
 
 
 function activeMenu(box, boxes, descriptions, attribute_names, box_class_basename, description_class_basename,
                     active_box_class, active_description_class) {
-    for (var i=0 ; i < boxes.length ; i++) {
+    for (let i=0 ; i < boxes.length ; i++) {
         boxes[i].classList.remove(active_box_class);
     }
-    for (var i=0 ; i < descriptions.length ; i++) {
+    for (let i=0 ; i < descriptions.length ; i++) {
         descriptions[i].classList.remove(active_description_class);
     }
-    for (var i=0 ; i < attribute_names.length ; i++) {
+    for (let i=0 ; i < attribute_names.length ; i++) {
         if(box.id === box_class_basename + '-' + attribute_names[i] + '') {
             document.getElementById(box_class_basename + '-' + attribute_names[i]).
             classList.add(active_box_class);
@@ -28,7 +28,7 @@ function activeMenu(box, boxes, descriptions, attribute_names, box_class_basenam
 
 
 // processing execution when a mouseover is detected on a special attribute box
-for (var i=0 ; i < special_attribute_boxes.length ; i++) {
+for (let i=0 ; i < special_attribute_boxes.length ; i++) {
     special_attribute_boxes[i].addEventListener('mouseover', function (evt) {
         activeMenu(this, special_attribute_boxes, special_attribute_descriptions, special_attribute_names,
             'special-attribute-box',
@@ -41,7 +41,7 @@ for (var i=0 ; i < special_attribute_boxes.length ; i++) {
 
 
 // processing execution when a mouseover is detected on a perk attribute box
-for (var i=0 ; i < perk_attribute_boxes.length ; i++) {
+for (let i=0 ; i < perk_attribute_boxes.length ; i++) {
     perk_attribute_boxes[i].addEventListener('mouseover', function (evt) {
         activeMenu(this, perk_attribute_boxes, perk_attribute_descriptions, perk_attribute_names,
             'perk-attribute-box',
