a83884a0de892d0b84761521a5d8715bcb08ade3
IAmTerror <cyrilberthout@gmail.com>
2021-03-14 21:42:01 +0100

    studies section

--- exp_studies.php
+++ exp_studies.php
@@ -3,56 +3,51 @@
         <div class="menu-attributes study-attributes">
             <div class="menu-attribute-box study-attribute-box study-attribute-box-active"
                  id="study-attribute-box-study_1">
-                <p class="menu-attribute study-attribute">DEV FULL STACK</p>
+                <p class="menu-attribute study-attribute">CDA</p>
             </div>
             <div class="menu-attribute-box study-attribute-box" id="study-attribute-box-study_2">
-                <p class="menu-attribute study-attribute">DEV BACK END</p>
+                <p class="menu-attribute study-attribute">BTS SIO SLAM</p>
             </div>
             <div class="menu-attribute-box study-attribute-box" id="study-attribute-box-study_3">
-                <p class="menu-attribute study-attribute">DEV BACK END</p>
+                <p class="menu-attribute study-attribute">DROIT 2EME ANNEE</p>
             </div>
             <div class="menu-attribute-box study-attribute-box" id="study-attribute-box-study_4">
-                <p class="menu-attribute study-attribute">SERVICE CIVIQUE</p>
+                <p class="menu-attribute study-attribute">DROIT 1ERE ANNEE</p>
             </div>
             <div class="menu-attribute-box study-attribute-box" id="study-attribute-box-study_5">
-                <p class="menu-attribute study-attribute">SERVICE CIVIQUE</p>
+                <p class="menu-attribute study-attribute">BAC ES</p>
             </div>
         </div>
         <div class="menu-right-column study-right-column">
             <p class="study-attribute-description study-attribute-description-active"
                id="study-attribute-description-study_1">
-                25 MARS 2019 - 17 MAI 2019<br />
-                Lycée Léonard de Vinci – Melun (77) : dans le cadre d’un stage professionnel, j’ai
-                développé de nouvelles fonctionnalités pour une application web Java / Spring
-                destinée à gérer les accès au réseau informatique d’un lycée (front & back office),
-                refactoré le code de l’existant, et renforcé la sécurité du système.
+                2017 - 2019<br />
+                GRETA MTE - Chennevières-sur-Marne (94) : dans le cadre de la préparation
+                du titre professionnel de Concepteur Développeur d’Applications ((NIV. II RNCP - 6 CEC)),
+                j’ai conçu et développé une application en Java / Spring destinée à gérer un cinéma de
+                quartier (front & back end)
             </p>
             <p class="study-attribute-description" id="study-attribute-description-study_2">
-                05 MARS 2018 - 27 AVRIL 2018<br />
-                Lycée Champlain – Chennevières-sur-Marne (94) : dans le cadre d’un stage
-                professionnel, j’ai participé à la conception d’une application web Java /Spring
-                destinée à gérer un questionnaire permettant de dépister le décrochage scolaire
-                de lycéens.
+                2016 - 2017<br />
+                GRETA MTE - Chennevières-sur-Marne (94) : dans le cadre du BTS SIO
+                (Services Informatiques aux Organisations) option SLAM (Solutions Logicielles et
+                Applications Métiers), j’ai étudié les bases de la conception et du développement
+                de programmes informatiques. Moyenne générale : 14,97/20
             </p>
             <p class="study-attribute-description" id="study-attribute-description-study_3">
-                MAI 2017 - JUIN 2017<br />
-                Lycée Champlain – Chennevières-sur-Marne (94) : dans le cadre d’un stage
-                professionnel, j’ai participé à la conception d’une application destinée à gérer les
-                conventions de stage des élèves d’un lycée
+                LICENCE DE DROIT (NIVEAU 2EME ANNÉE)<br />
+                2009 - 2010<br />
+                Université Val d’Essonne – Évry (91)
             </p>
             <p class="study-attribute-description" id="study-attribute-description-study_4">
-                UNIS CITÉ<br />
-                SEPT 2008 - JUIL 2009<br />
-                Relais Formation Emploi - Évry (91) : création d’ateliers santé sur le thème des addictions. <br />
-                Nouvelles Voies - Île-de-France : constitution et suivi de dossiers administratifs et juridiques,
-                création d’outils pédagogiques destinés à des ateliers santé.
+                LICENCE DE DROIT (NIVEAU 1ERE ANNÉE)<br />
+                2004 - 2005<br />
+                Université Paris V – Malakoff (92)
             </p>
             <p class="study-attribute-description" id="study-attribute-description-study_5">
-                UNIS CITÉ<br />
-                SEPT 2008 - JUIL 2009<br />
-                Relais Formation Emploi - Évry (91) : création d’ateliers santé sur le thème des addictions. <br />
-                Nouvelles Voies - Île-de-France : constitution et suivi de dossiers administratifs et juridiques,
-                création d’outils pédagogiques destinés à des ateliers santé.
+                BACCALAURÉAT ÉCONOMIQUE ET SOCIAL<br />
+                2003 - 2004<br />
+                Lycée Rabelais – Meudon (92)
             </p>
         </div>
     </div>
