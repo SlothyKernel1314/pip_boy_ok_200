361a36ff7cace70b3145faabf21a38c3a88d9eb3
IAmTerror <cyrilberthout@gmail.com>
2021-03-14 13:36:45 +0100

    jobs section

--- exp_jobs.php
+++ exp_jobs.php
@@ -2,40 +2,26 @@
     <div class="job-container">
         <div class="job-attributes">
             <div class="job-attribute-box job-attribute-box-active" id="job-attribute-box-job_1">
-                <p class="job-attribute">job 1</p>
+                <p class="job-attribute">DEV FULL STACK</p>
             </div>
             <div class="job-attribute-box" id="job-attribute-box-job_2">
-                <p class="job-attribute">job 2</p>
+                <p class="job-attribute">DEV BACK END</p>
             </div>
             <div class="job-attribute-box" id="job-attribute-box-job_3">
-                <p class="job-attribute">job 3</p>
+                <p class="job-attribute">DEV BACK END</p>
             </div>
             <div class="job-attribute-box" id="job-attribute-box-job_4">
-                <p class="job-attribute">job 4</p>
+                <p class="job-attribute">SERVICE CIVIQUE</p>
             </div>
         </div>
         <div class="job-right-column">
-            <!-- images ----------------------------------------------------------------------------------------------->
-            <img src="img/perk/chemist.png"
-                 class="job-attribute-image job-attribute-image-active"
-                 id="job-attribute-image-job_1"
-                 alt="">
-            <img src="img/perk/cannibal.png"
-                 class="job-attribute-image"
-                 id="job-attribute-image-job_2"
-                 alt="">
-            <img src="img/perk/fortune_finder.png"
-                 class="job-attribute-image"
-                 id="job-attribute-image-job_3"
-                 alt="">
-            <img src="img/perk/idiot_savant.png"
-                 class="job-attribute-image"
-                 id="job-attribute-image-job_4"
-                 alt="">
-            <!-- descriptions ----------------------------------------------------------------------------------------->
             <p class="job-attribute-description job-attribute-description-active"
                id="job-attribute-description-job_1">
-                Desc job 1
+                25 MARS 2019 - 17 MAI 2019<br />
+                Lycée Léonard de Vinci – Melun (77) : dans le cadre d’un stage professionnel, j’ai
+                développé de nouvelles fonctionnalités pour une application web Java / Spring
+                destinée à gérer les accès au réseau informatique d’un lycée (front & back office),
+                refactoré le code de l’existant, et renforcé la sécurité du système.
             </p>
             <p class="job-attribute-description" id="job-attribute-description-job_2">
                 Desc job 2
