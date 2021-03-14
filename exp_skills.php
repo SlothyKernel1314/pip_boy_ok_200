ea159380f9414fd60a3eae4fe7112a81ecbcc1b9
IAmTerror <cyrilberthout@gmail.com>
2021-03-14 22:16:38 +0100

    skills section

--- exp_skills.php
+++ exp_skills.php
@@ -3,68 +3,56 @@
         <div class="menu-attributes skill-attributes">
             <div class="menu-attribute-box skill-attribute-box skill-attribute-box-active"
                  id="skill-attribute-box-skill_1">
-                <p class="menu-attribute skill-attribute">CDA</p>
+                <p class="menu-attribute skill-attribute">Frameworks</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_2">
-                <p class="menu-attribute skill-attribute">BTS SIO SLAM</p>
+                <p class="menu-attribute skill-attribute">Langages</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_3">
-                <p class="menu-attribute skill-attribute">DROIT 2EME ANNEE</p>
+                <p class="menu-attribute skill-attribute">Front-end</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_4">
-                <p class="menu-attribute skill-attribute">DROIT 1ERE ANNEE</p>
+                <p class="menu-attribute skill-attribute">Bases de données</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_5">
-                <p class="menu-attribute skill-attribute">BAC ES</p>
+                <p class="menu-attribute skill-attribute">Conception</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_6">
-                <p class="menu-attribute skill-attribute">BAC ES</p>
+                <p class="menu-attribute skill-attribute">Environnement</p>
             </div>
             <div class="menu-attribute-box skill-attribute-box" id="skill-attribute-box-skill_7">
-                <p class="menu-attribute skill-attribute">BAC ES</p>
+                <p class="menu-attribute skill-attribute">Toolbox</p>
             </div>
         </div>
         <div class="menu-right-column skill-right-column">
             <p class="skill-attribute-description skill-attribute-description-active"
                id="skill-attribute-description-skill_1">
-                2017 - 2019<br />
-                GRETA MTE - Chennevières-sur-Marne (94) : dans le cadre de la préparation
-                du titre professionnel de Concepteur Développeur d’Applications ((NIV. II RNCP - 6 CEC)),
-                j’ai conçu et développé une application en Java / Spring destinée à gérer un cinéma de
-                quartier (front & back end)
+                Développement d’applications web : j’ai étudié durant ma formation le framework
+                Spring afin de développer des applications web, ainsi que de nombreux modules
+                et technologies associées : Spring Boot, JPA, Hibernate, Maven, Spring Security...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_2">
-                2016 - 2017<br />
-                GRETA MTE - Chennevières-sur-Marne (94) : dans le cadre du BTS SIO
-                (Services Informatiques aux Organisations) option SLAM (Solutions Logicielles et
-                Applications Métiers), j’ai étudié les bases de la conception et du développement
-                de programmes informatiques. Moyenne générale : 14,97/20
+                Langages de programmation : utilisation courante de Java et Python dans le
+                cadre de projets professionnels ou personnels, notions en Lua, C#, JavaScript,
+                Bash...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_3">
-                LICENCE DE DROIT (NIVEAU 2EME ANNÉE)<br />
-                2009 - 2010<br />
-                Université Val d’Essonne – Évry (91)
+                Front-end : HTML, CSS, jQuery, Semantic UI, Bootstrap...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_4">
-                LICENCE DE DROIT (NIVEAU 1ERE ANNÉE)<br />
-                2004 - 2005<br />
-                Université Paris V – Malakoff (92)
+                Bases de données : PostgreSQL, MySQL, LDAP...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_5">
-                BACCALAURÉAT ÉCONOMIQUE ET SOCIAL<br />
-                2003 - 2004<br />
-                Lycée Rabelais – Meudon (92)
+                Conception : analyse des besoins, rédaction d’un cahier des charges, langage UML...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_6">
-                BACCALAURÉAT ÉCONOMIQUE ET SOCIAL<br />
-                2003 - 2004<br />
-                Lycée Rabelais – Meudon (92)
+                Environnement : GNU / Linux, Windows, IntelliJ IDEA, conteneurisation sous
+                Docker, virtualisation sous VMware ou VirtualBox, gestion de versions sous Git...
             </p>
             <p class="skill-attribute-description" id="skill-attribute-description-skill_7">
-                BACCALAURÉAT ÉCONOMIQUE ET SOCIAL<br />
-                2003 - 2004<br />
-                Lycée Rabelais – Meudon (92)
-            </p>
+                Toolbox : réalisation de prototypes de jeux vidéo sous Love2D (Lua), conception
+                d’architectures REST, utilisation d’API tierces, création d’ETL (Extract-Transform-Load),
+                rédaction de scripts système, notions de pentesting...
         </div>
     </div>
 </section>
