081fcf7ef4713a846ef7272a9e4dad6b93c6d186
IAmTerror <cyrilberthout@gmail.com>
2021-03-15 18:48:15 +0100

    color section

--- color.php
+++ color.php
@@ -1,3 +1,19 @@
 <section class="core-display" id="color-section">
-    <p>test js COLOR</p>
+    <div class="menu-container color-container">
+        <div class="color-attributes">
+            <div class="color-attribute-box color-attribute-box-active"
+                 id="color-attribute-box-color_1">
+                <p class="color-attribute">Color 1</p>
+            </div>
+            <div class="color-attribute-box" id="color-attribute-box-color_2">
+                <p class="color-attribute">Color 2</p>
+            </div>
+            <div class="color-attribute-box" id="color-attribute-box-color_3">
+                <p class="color-attribute">Color 3</p>
+            </div>
+            <div class="color-attribute-box" id="color-attribute-box-color_4">
+                <p class="color-attribute">Color 4</p>
+            </div>
+        </div>
+    </div>
 </section>
