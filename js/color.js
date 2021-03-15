64d45fd29497ea238fe0adcd5877ba3feb8cee09
IAmTerror <cyrilberthout@gmail.com>
2021-03-15 22:52:12 +0100

    color section

--- js/color.js
+++ js/color.js
@@ -4,9 +4,9 @@ let color_class_active_name = 'color-box-active';
 let color_names = ['green_default', 'blue_nuka_cola', 'color_3', 'color_4', 'color_5', 'color_6', 'color_7', 'color_8',
     'color_9', 'color_10', 'color_11', 'color_12'];
 
-let green_default_set_color = ['green_default', '#18dc0c', '#11291b']
-let blue_nuka_cola = ['blue_nuka_cola', '#2eceff', '#11291b']
-let color_themes = [green_default_set_color, blue_nuka_cola]
+let green_default_set_color = ['green_default', '#18dc0c', '#11291b'];
+let blue_nuka_cola = ['blue_nuka_cola', '#2eceff', '#11291b'];
+let color_themes = [green_default_set_color, blue_nuka_cola];
 
 function activeColor(box, boxes, attribute_names,
                      box_class_basename, active_box_class, color_themes) {
@@ -17,7 +17,7 @@ function activeColor(box, boxes, attribute_names,
         if(box.id === box_class_basename + '-' + attribute_names[i] + '') {
             document.getElementById(box_class_basename + '-' + attribute_names[i]).
             classList.add(active_box_class);
-            document.documentElement.style.setProperty('--pip-boy-primary-color', color_themes[1][1]);
+            document.documentElement.style.setProperty('--pip-boy-primary-color', color_themes[i][1]);
         }
     }
 }
@@ -28,6 +28,5 @@ for (let i=0 ; i < color_boxes.length ; i++) {
     color_boxes[i].addEventListener('click', function (evt) {
         activeColor(this, color_boxes, color_names,
             color_class_basename, color_class_active_name, color_themes)
-
     })
 }
