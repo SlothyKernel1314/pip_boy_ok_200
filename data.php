1aece85f2906a1d39c7c9398a4b0b90ea73e0453
IAmTerror <cyrilberthout@gmail.com>
2021-03-18 18:07:34 +0100

    Revert "data section : content WIP"
    This reverts commit 35e308c8bd61dc2669011ff2081c4aae1d0d3c6a.

--- data.php
+++ data.php
@@ -3,16 +3,16 @@
         <div class="menu-attributes data-attributes">
             <div class="menu-attribute-box data-attribute-box data-attribute-box-active"
                  id="data-attribute-box-data_1">
-                <p class="menu-attribute data-attribute">Profil</p>
+                <p class="menu-attribute data-attribute">Data 1</p>
             </div>
             <div class="menu-attribute-box data-attribute-box" id="data-attribute-box-data_2">
-                <p class="menu-attribute data-attribute">Poste</p>
+                <p class="menu-attribute data-attribute">Data 2</p>
             </div>
             <div class="menu-attribute-box data-attribute-box" id="data-attribute-box-data_3">
-                <p class="menu-attribute data-attribute">Contact</p>
+                <p class="menu-attribute data-attribute">Data 3</p>
             </div>
             <div class="menu-attribute-box data-attribute-box" id="data-attribute-box-data_4">
-                <p class="menu-attribute data-attribute">Langues</p>
+                <p class="menu-attribute data-attribute">Data 4</p>
             </div>
             <div class="menu-attribute-box data-attribute-box" id="data-attribute-box-data_5">
                 <p class="menu-attribute data-attribute">Data 5</p>
@@ -27,22 +27,17 @@
         <div class="menu-right-column data-right-column">
             <p class="data-attribute-description data-attribute-description-active"
                id="data-attribute-description-data_1">
-                Cyril BERTHOUT, 37 ans<br /><br />
-                Rigoureux, logique et autonome, je suis un développeur Java / Spring
-                diplômé, à la recherche d’un emploi dans le domaine du développement
-                d’applications web. Curieux, je pratique aussi la programmation durant
-                mon temps libre afin de découvrir de nouveaux outils
+                Desc 1
             </p>
             <p class="data-attribute-description" id="data-attribute-description-data_2">
-                Concepteur développeur d'applications
+                Desc 2
             </p>
             <p class="data-attribute-description" id="data-attribute-description-data_3">
-                Adresse : 94000 CRETEIL<br /><br />
-                Email : cyrilberthout@gmail.com<br /><br />
+                Desc 3
             </p>
             <p class="data-attribute-description" id="data-attribute-description-data_4">
-                Français<br />
-                Anglais (B2 CECRL)</p>
+                Desc 4
+            </p>
             <p class="data-attribute-description" id="data-attribute-description-data_5">
                 Desc 5
             </p>
