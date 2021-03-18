ae77310e76cd7598bc4686e3ca209b00c1d774e9
IAmTerror <cyrilberthout@gmail.com>
2021-03-18 17:05:17 +0100

    color section

--- js/color.js
+++ js/color.js
@@ -1,5 +1,6 @@
 let info_images = document.querySelectorAll('.info-icon > img');
 let menu_images = document.querySelectorAll('.menu-attribute-image');
+let vault_boy_walking_image = document.getElementById('vault-boy-walking');
 let color_boxes = document.querySelectorAll('.color-box');
 let color_class_basename = 'color-box';
 let color_class_active_name = 'color-box-active';
@@ -11,37 +12,48 @@ let color_class_active_name = 'color-box-active';
  * @var 3: --pip-boy-secondary-color
  * @var 4: CCS filter property for info_images
  * @var 5: CCS filter property for menu_images
+ * @var 6: CCS filter property for vault boy walking image
  */
 let green_default = ['green_default', '#18dc0c', '#11291b',
-                        'grayscale(100%) brightness(80%) sepia(100%) hue-rotate(50deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(50deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(80%) sepia(100%) hue-rotate(50deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(50deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(50deg) saturate(1500%) contrast(0.99)'];
 let blue_nuka_cola = ['blue_nuka_cola', '#2eceff', '#002733',
-                        'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(500deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(500deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(500deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(500deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(500deg) saturate(1500%) contrast(0.99)'];
 let orange_mojave = ['orange_mojave', '#ffb641', '#1a1000',
-                        'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(90%) sepia(100%) hue-rotate(10deg) saturate(1500%) contrast(0.99)'];
 let purple = ['purple', '#8438f5', '#150330',
-                        'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(200deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(30%) sepia(100%) hue-rotate(210deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(200deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(30%) sepia(100%) hue-rotate(210deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(220deg) saturate(1500%) contrast(0.99)'];
 let red = ['red', '#bb313d', '#280b0d',
-                        'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(310deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(20%) sepia(100%) hue-rotate(310deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(310deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(20%) sepia(100%) hue-rotate(310deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(310deg) saturate(1000%) contrast(0.99)'];
 let green_mutant = ['green_mutant', '#1aff80', '#003317',
-                        'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(470deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(470deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(470deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(470deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(470deg) saturate(1500%) contrast(0.99)'];
 let pink_boy = ['pink_boy', '#ff00ff', '#330033',
-                        'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(220deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(30%) sepia(100%) hue-rotate(250deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(220deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(30%) sepia(100%) hue-rotate(250deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(250deg) saturate(1500%) contrast(0.99)'];
 let night_blue = ['night_blue', '#0897ff', '#001e33',
-                        'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(180deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(180deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(70%) sepia(100%) hue-rotate(180deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(180deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(50%) sepia(100%) hue-rotate(150deg) saturate(1500%) contrast(0.99)'];
 let ice = ['ice', '#dce0e1', '#171b1c',
-                        'grayscale(100%) brightness(100%) sepia(0%) hue-rotate(100deg) saturate(10%) contrast(0.9)',
-                            'grayscale(100%) brightness(90%) sepia(0%) hue-rotate(100deg) saturate(10%) contrast(0.8)'];
+            'grayscale(100%) brightness(100%) sepia(0%) hue-rotate(100deg) saturate(10%) contrast(0.9)',
+            'grayscale(100%) brightness(90%) sepia(0%) hue-rotate(100deg) saturate(10%) contrast(0.8)',
+            'grayscale(100%) brightness(90%) sepia(90%) hue-rotate(100deg) saturate(10%) contrast(0.99)'];
 let gold = ['gold', '#ffd700', '#332b00',
-                        'grayscale(100%) brightness(90%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
-                        'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)'];
+            'grayscale(100%) brightness(90%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(60%) sepia(100%) hue-rotate(10deg) saturate(1000%) contrast(0.8)',
+            'grayscale(100%) brightness(80%) sepia(100%) hue-rotate(15deg) saturate(2000%) contrast(0.99)'];
 
 let color_themes = [green_default, blue_nuka_cola, orange_mojave, purple, red,
     green_mutant, pink_boy, night_blue, ice, gold];
@@ -49,7 +61,7 @@ let color_themes = [green_default, blue_nuka_cola, orange_mojave, purple, red,
 
 function activeColor(box, boxes, color_themes,
                      box_class_basename, active_box_class,
-                     info_images, menu_images) {
+                     info_images, menu_images, vault_boy_walking_image) {
     for (let i=0 ; i < boxes.length ; i++) {
         boxes[i].classList.remove(active_box_class);
     }
@@ -70,6 +82,9 @@ function activeColor(box, boxes, color_themes,
                 menu_images[j].style.removeProperty('filter');
                 menu_images[j].style.setProperty('filter', color_themes[i][4]);
             }
+            // setting vault boy image color with filter CSS property
+            vault_boy_walking_image.style.removeProperty('filter');
+            vault_boy_walking_image.style.setProperty('filter', color_themes[i][5]);
         }
     }
 }
@@ -80,6 +95,6 @@ for (let i=0 ; i < color_boxes.length ; i++) {
     color_boxes[i].addEventListener('click', function (evt) {
         activeColor(this, color_boxes, color_themes,
             color_class_basename, color_class_active_name,
-            info_images, menu_images)
+            info_images, menu_images, vault_boy_walking_image)
     })
 }
