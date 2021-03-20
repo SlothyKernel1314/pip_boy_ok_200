141e3d3df187adb0c07c41bb596e1b15e3174484
IAmTerror <cyrilberthout@gmail.com>
2021-03-20 21:09:44 +0100

    map section

--- map.php
+++ map.php
@@ -1,5 +1,7 @@
 <section class="core-display" id="map-section">
-    <img src="img/idf_map.png"
-         id="idf-map"
-         alt="a map of Ile-de-France french region">
+    <div class="menu-container map-container">
+        <img src="img/idf_map.png"
+             id="idf-map"
+             alt="a map of Ile-de-France french region">
+    </div>
 </section>
